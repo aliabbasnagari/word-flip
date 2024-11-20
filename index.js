fc3859f45247b75274d032b50d636b9d53815f77
aliabbasnagari <aliabbasinayatali@gmail.com>
2024-11-21 01:18:46 +0500

    dance animation

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var ACTIVE_CARDS = [];
 var WORD_PAIRS = [];
 var PAIRS_FOUND = 0;
 
+var dance_duration = 1000;
+
 
 function pairExists(pair, pairs_list) {
     return pairs_list.includes(`${pair[0]} ${pair[1]}`) || pairs_list.includes(`${pair[1]} ${pair[0]}`);
@@ -56,28 +58,51 @@ function flipCard(card) {
         ACTIVE_CARDS = ACTIVE_CARDS.filter(C => C !== card);
     }
 
-    if (ACTIVE_CARDS.length == 2 && pairExists([ACTIVE_CARDS[0].word, ACTIVE_CARDS[1].word], WORD_PAIRS)) {
-        console.log("FOUNDED");
-        PAIRS_FOUND++;
-        if (PAIRS_FOUND == WORD_PAIRS.length) {
-            let concatenated_words = WORD_PAIRS.map(word => word).join(" <br> ");
-            let title = popup.querySelector('h2');
-            let message = popup.querySelector('p');
-            title.innerHTML = "&#127881 Nice Going!";
-            message.innerHTML = `Congrats! You solved the puzzle. <br><br> <b>${concatenated_words}</b> `;
-            popup.style.display = 'block';
-            console.log('WON');
-        }
-
-        ACTIVE_CARDS[0].cardBack.style.opacity = 0.3;
-        ACTIVE_CARDS[1].cardBack.style.opacity = 0.3;
-
-        ACTIVE_CARDS[0].dataset.face = 2;
-        ACTIVE_CARDS[1].dataset.face = 2;
-        ACTIVE_CARDS = [];
-    }
 
     console.log(ACTIVE_CARDS);
+
+    if (ACTIVE_CARDS.length == 2) {
+
+        if (pairExists([ACTIVE_CARDS[0].word, ACTIVE_CARDS[1].word], WORD_PAIRS)) {
+            console.log("FOUNDED");
+
+            PAIRS_FOUND++;
+            if (PAIRS_FOUND == WORD_PAIRS.length) {
+                let concatenated_words = WORD_PAIRS.map(word => word).join(" <br> ");
+                let title = popup.querySelector('h2');
+                let message = popup.querySelector('p');
+                title.innerHTML = "&#127881 Nice Going!";
+                message.innerHTML = `Congrats! You solved the puzzle. <br><br> <b>${concatenated_words}</b> `;
+                popup.style.display = 'block';
+
+                console.log('WON');
+            }
+
+            ACTIVE_CARDS[0].cardBack.style.opacity = 0.3;
+            ACTIVE_CARDS[1].cardBack.style.opacity = 0.3;
+
+            ACTIVE_CARDS[0].dataset.face = 2;
+            ACTIVE_CARDS[1].dataset.face = 2;
+
+            ACTIVE_CARDS[0].classList.add("dance");
+            ACTIVE_CARDS[1].classList.add("dance");
+            setTimeout(() => ACTIVE_CARDS = [], dance_duration);
+
+        } else {
+            console.log("NOT FOUNDED");
+            setTimeout(() => {
+                if (ACTIVE_CARDS[0]) {
+                    ACTIVE_CARDS[0].classList.toggle("flip");
+                    ACTIVE_CARDS[0].dataset.face = 0;
+                }
+                if (ACTIVE_CARDS[1]) {
+                    ACTIVE_CARDS[1].classList.toggle("flip");
+                    ACTIVE_CARDS[1].dataset.face = 0;
+                }
+                ACTIVE_CARDS = [];
+            }, 2000);
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -103,6 +128,8 @@ document.addEventListener('DOMContentLoaded', () => {
     var card_container = document.getElementById("cardContainer");
     card_container.style.gridTemplateColumns = 'repeat(4, 1fr)';
 
+    var message = document.getElementById("wflipMessages");
+
     function loadGame(game_size) {
         GAME_SIZE = game_size;
         ACTIVE_CARDS = [];
@@ -112,6 +139,7 @@ document.addEventListener('DOMContentLoaded', () => {
         card_container.replaceChildren();
         card_container.style.gridTemplateColumns = `repeat(${GAME_SIZE[1]}, 1fr)`;
 
+        message.innerHTML = '<h3> Loading... </h3>';
         fetch(`https://mytoronto.thedev.ca/wp-json/words/v1/random-words/?count=${(GAME_SIZE[0] * GAME_SIZE[1]) / 2}`)
             .then(response => {
                 if (!response.ok) {
@@ -120,15 +148,23 @@ document.addEventListener('DOMContentLoaded', () => {
                 return response.json();
             })
             .then(words_json => {
-                WORD_PAIRS = words_json.words;
-                console.log(WORD_PAIRS);
+                try {
+                    WORD_PAIRS = words_json.words;
+                    console.log(WORD_PAIRS);
 
-                var words = shuffle(WORD_PAIRS.flatMap(item => item.split(" ")));
-                console.log(words);
+                    var words = shuffle(WORD_PAIRS.flatMap(item => item.split(" ")));
+                    console.log(words);
 
-                words.forEach(word => {
-                    createCard(word, card_container);
-                });
+                    words.forEach(word => {
+                        createCard(word, card_container);
+                    });
+                    message.innerHTML = '';
+                } catch (error) {
+                    throw new Error(`Error creating cards: ${error.message}`);
+                }
+            }).catch(error => {
+                console.error(error);
+                message.innerHTML = `<h3>Error: ${error.message}</h3>`;
             });
     }
 
